b39c1f041a7ee6a8f30e12a5550b624557161253
Sajjon <alex.cyon@gmail.com>
2019-10-04 22:17:40 +0200

    Conforming wrapper to AdditiveArithmetic and ExpressibleByIntegerLiteral and Equatable

--- Sources/AnyBinaryInteger/AnyBinaryInteger.swift
+++ Sources/AnyBinaryInteger/AnyBinaryInteger.swift
@@ -1,3 +1,85 @@
-struct AnyBinaryInteger {
-    var text = "Hello, World!"
+import BigInt
+
+// BinaryInteger
+// AdditiveArithmetic
+// Numeric
+
+public typealias BigNumber = AnyAdditiveArithmetic<BigInt>
+
+public struct AnyAdditiveArithmetic<Magnitude>: AdditiveArithmetic & ExpressibleByIntegerLiteral where Magnitude: BinaryInteger {
+
+    public let magnitude: Magnitude
+
+    public init(magnitude: Magnitude) {
+        self.magnitude = magnitude
+    }
+}
+
+private extension AnyAdditiveArithmetic {
+
+    static func forward(
+        _ lhs: Self, _ rhs: Self,
+        _ combine: @escaping (Magnitude, Magnitude) -> Magnitude
+    ) -> Self {
+        let combinedMagnitude: Magnitude = forwardMagnitude(lhs, rhs, combine)
+        return Self(magnitude: combinedMagnitude)
+    }
+
+    static func forwardMagnitude<Result>(
+        _ lhs: Self, _ rhs: Self,
+        _ combine: @escaping (Magnitude, Magnitude) -> Result
+    ) -> Result {
+        return forwardMapCombine(lhs, rhs, mapper: { $0.magnitude }, combine: combine)
+    }
+
+    static func forwardMapCombine<Result, Value>(
+        _ lhs: Self, _ rhs: Self,
+        mapper: (Self) -> Value,
+        combine: (Value, Value) -> Result
+    ) -> Result {
+        return combine(mapper(lhs), mapper(rhs))
+    }
+
+}
+
+// MARK: - Equatable
+public extension AnyAdditiveArithmetic {
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return forwardMagnitude(lhs, rhs, ==)
+    }
+}
+
+// MARK: - AdditiveArithmetic
+public extension AnyAdditiveArithmetic {
+
+    /// The zero value.
+    static var zero: Self { .init(magnitude: .zero) }
+
+    /// Adds two values and produces their sum.
+    static func + (_ lhs: Self, _ rhs: Self) -> Self {
+        forward(lhs, rhs, +)
+    }
+
+    /// Adds two values and stores the result in the left-hand-side variable.
+    static func += (_ lhs: inout Self, _ rhs: Self) {
+        lhs = (lhs + rhs)
+    }
+
+    /// Subtracts one value from another and produces their difference.
+    static func - (_ lhs: Self, _ rhs: Self) -> Self {
+        forward(lhs, rhs, -)
+    }
+
+    /// Subtracts the second value from the first and stores the difference in the left-hand-side variable.
+    static func -= (_ lhs: inout Self, _ rhs: Self) {
+        lhs = (lhs - rhs)
+    }
+}
+
+// MARK: - ExpressibleByIntegerLiteral
+public extension AnyAdditiveArithmetic {
+    init(integerLiteral value: Magnitude.IntegerLiteralType) {
+        self.init(magnitude: Magnitude(integerLiteral: value))
+    }
 }
--- Tests/AnyBinaryIntegerTests/AnyBinaryIntegerTests.swift
+++ Tests/AnyBinaryIntegerTests/AnyBinaryIntegerTests.swift
@@ -2,14 +2,35 @@ import XCTest
 @testable import AnyBinaryInteger
 
 final class AnyBinaryIntegerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AnyBinaryInteger().text, "Hello, World!")
+
+    func testEquatable() {
+        XCTAssertEqual(BigNumber(magnitude: 1), BigNumber(magnitude: 1))
+        XCTAssertNotEqual(BigNumber(magnitude: 1), BigNumber(magnitude: 2))
+    }
+
+    func testAddition() {
+        doTest(1, 1, expectedResult: 2, +)
+        doTest(2, 3, expectedResult: 5, +)
+    }
+
+    func testSubtraction() {
+        doTest(7, 5, expectedResult: 2, -)
+        doTest(7, 7, expectedResult: 0, -)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testEquatable", testEquatable),
+        ("testAddition", testAddition),
+        ("testSubtraction", testSubtraction),
     ]
+}
+
+private extension AnyBinaryIntegerTests {
+    func doTest<Result>(
+        _ lhs: BigNumber, _ rhs: BigNumber,
+        expectedResult: Result,
+        _ combine: (BigNumber, BigNumber) -> Result
+    ) where Result: Equatable {
+        XCTAssertEqual(combine(lhs, rhs), expectedResult)
+    }
 }
